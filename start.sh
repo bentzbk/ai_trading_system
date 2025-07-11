3b08d699cb464dd864eedf385ea59c027e4edb74
bentzbk <benbaichman@gmail.com>
2025-07-11 14:49:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 export PORT=${PORT:-8080}
 export HOST=${HOST:-0.0.0.0}
 export PYTHONUNBUFFERED=1
 
 mkdir -p /app/logs
 
-echo "Starting Gradio server..."
-exec python huggingface_deploy.py
+echo "Starting Flask server..."
+exec python app.py
